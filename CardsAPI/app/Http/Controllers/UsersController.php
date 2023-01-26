a81e8da1682481e0b0feed9a880fab8aaae95999
PabloGDP <113938458+PabloGonzalvezDelPino@users.noreply.github.com>
2023-01-26 16:51:15 +0100

    User Skeleton

---
+++ CardsAPI/app/Http/Controllers/UsersController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use App\Http\Helpers\ResponseGenerator;
+use App\Models\User;
+
+class UsersController extends Controller
+{
+    //
+}
--- CardsAPI/routes/api.php
+++ CardsAPI/routes/api.php
@@ -13,7 +13,10 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
+Route::prefix('/users')->group(function(){
+    Route::post('/login', [UsersController::class, 'login']);
+    Route::put('/register', [UsersController::class, 'register']);
+});
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
