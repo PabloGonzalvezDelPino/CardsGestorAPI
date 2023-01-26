99b7fd2ef6d4b22569bba9f3bd39d4ccf8a1e8fa
PabloGDP <113938458+PabloGonzalvezDelPino@users.noreply.github.com>
2023-01-26 18:03:16 +0100

    User Login Finished

--- CardsAPI/app/Http/Controllers/UsersController.php
+++ CardsAPI/app/Http/Controllers/UsersController.php
@@ -39,4 +39,37 @@ class UsersController extends Controller
             return ResponseGenerator::generateResponse("KO", 500, null, "Datos no registrados");
         }
     }
+    public function login(Request $request){
+        $json = $request->getContent();
+        $data = json_decode($json);
+
+        if($data){
+           $validate = Validator::make(json_decode($json,true), [
+               'username' => 'required',
+               'password' => 'required'
+           ]);
+           if($validate->fails()){
+               return ResponseGenerator::generateResponse("OK", 422, null, $validate->errors());
+           }else{
+               try{
+                   $user = User::where('username', 'like', $data->username)->firstOrFail();
+
+                   if(!Hash::check($data->password, $user->password)) {
+                       return ResponseGenerator::generateResponse("KO", 404, null, "Login incorrecto, comprueba la contraseña");
+                   }else{
+                       $user->tokens()->delete();
+   
+                       $token = $user->createToken($user->username);
+                       return ResponseGenerator::generateResponse("OK", 200, $token->plainTextToken, "Login correcto");
+                   }
+               }catch(\Exception $e){
+                   return ResponseGenerator::generateResponse("KO", 404, null, "Login incorrecto, usuario erróneo");
+               }
+           }
+       }
+   }
+   //User aa token: hgPUxmfA09Qr3BdGnmRgIhX9qWHkKO6NwTkQx9gX
+   public function recoverPass(Request $request){
+
+   }
 }
