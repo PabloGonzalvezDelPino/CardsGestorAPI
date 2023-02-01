16bd94b11c23f6cea2250f8eb45ca3d59f47b729
PabloGDP <113938458+PabloGonzalvezDelPino@users.noreply.github.com>
2023-02-01 15:57:21 +0100

    Cards update

--- CardsAPI/app/Http/Controllers/CollectionsController.php
+++ CardsAPI/app/Http/Controllers/CollectionsController.php
@@ -4,6 +4,11 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Hash;
+use App\Http\Helpers\ResponseGenerator;
+use App\Models\Collection;
+
 class CollectionsController extends Controller
 {
     //
